2b789bb89d0cdae0f407ed9e685110c279d0ae45
Vimal Makenthirarasa <vmaken927@gmail.com>
2024-12-13 13:30:22 -0500

    Update Main.java

--- core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/Main.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/Main.java
@@ -8,21 +8,26 @@ import ca.jrvs.apps.stockquote.service.PositionService;
 import ca.jrvs.apps.stockquote.service.QuoteService;
 import okhttp3.OkHttpClient;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+// -it flag keeps stdin open 
+/**
+ * docker run --rm -it \
+ * -v "$(pwd)/log:/log" \
+ * ${docker_user}/jdbc
+ */
+
 public class Main {
 
     public static void main(String[] args) {
         Map<String, String> properties = new HashMap<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/properties.txt"))) {
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(Main.class.getClassLoader().getResourceAsStream("properties.txt")))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] tokens = line.split(":");
